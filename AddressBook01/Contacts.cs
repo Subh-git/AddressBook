53b2f15c09fadd71113430cb52e0341e8dcd8c21
Subhadeep <subhadeep0326@gmail.com>
2021-10-18 21:32:32 +0530

    Subhadeep added the UC1 test case for the details of a single person

---
+++ AddressBook01/Contacts.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook01
+{
+   public class Contacts
+    {
+        public string firstName;
+        public string lastName;
+        public string address;
+        public string phoneNumber;
+        public string email;
+        public string city;
+        public string state;
+        public string zip;
+
+        public Contacts(string firstName, string lastName, string address, string phoneNumber, string email, string city, string state, string zip )
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+            this.city = city;
+            this.state = state;
+            this.zip = zip;
+
+
+        }
+    }
+}
--- AddressBook01/Program.cs
+++ AddressBook01/Program.cs
@@ -6,7 +6,8 @@ namespace AddressBook01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Address book problem");
+            Contacts myobj = new Contacts("Subhadeep", "Bhattacharjee", "Dohabora chuk", "123456", "subh@gmail.com", "jorhat", "assam", "111111");
+            Console.WriteLine("First Name: {0} Last Name: {1} Address: {2} Phone Number: {3} Email: {4} City: {5} State: {6} ZipCode: {7}",myobj.firstName,myobj.lastName, myobj.address, myobj.phoneNumber, myobj.email,myobj.city,myobj.state,myobj.zip);
         }
     }
 }
