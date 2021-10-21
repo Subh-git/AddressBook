e13e84eb28c6ab085ee7b55adefbdbf651f67ef2
Subhadeep <subhadeep0326@gmail.com>
2021-10-21 10:30:02 +0530

    Subhadeep added the feature of editing in the address book

--- AddressBook01/AddressBook.cs
+++ AddressBook01/AddressBook.cs
@@ -6,45 +6,98 @@ using System.Threading.Tasks;
 
 namespace AddressBook01
 {
+    /// <summary>
+    /// This is the main address book class that has all the methods defined in it.
+    /// </summary>
     class AddressBook
     {
-       public List<Contacts> contact = new List<Contacts>();
+        //declaring a list with the class Contacts as the type
+        public List<Contacts> contact = new List<Contacts>();
         public void AddAddress()
         {
+            //creating a new object contactBook of the class Contacts to add addressess
             Contacts contactBook = new Contacts();
-            Console.WriteLine("Enter First Name");
+            Console.Write("Enter First Name - ");
             contactBook.firstName = Console.ReadLine();
-            Console.WriteLine("Enter Last Name");
+            Console.Write("Enter Last Name - ");
             contactBook.lastName = Console.ReadLine();
-            Console.WriteLine("Enter Address");
+            Console.Write("Enter Address - ");
             contactBook.address = Console.ReadLine();
-            Console.WriteLine("Enter Phone number");
+            Console.Write("Enter Phone number - ");
             contactBook.phoneNumber = Console.ReadLine();
-            Console.WriteLine("Enter Email ID");
+            Console.Write("Enter Email ID - ");
             contactBook.email = Console.ReadLine();
-            Console.WriteLine("Enter City");
+            Console.Write("Enter City - ");
             contactBook.city = Console.ReadLine();
-            Console.WriteLine("Enter State");
+            Console.Write("Enter State - ");
             contactBook.state = Console.ReadLine();
-            Console.WriteLine("Enter ZIP code");
+            Console.Write("Enter ZIP code - ");
             contactBook.zip = Console.ReadLine();
 
+            //Addidng to the list
             contact.Add(contactBook);
         }
 
-        public void View()
+        public void View()                                              //this is  the method to view all the contacts stored currently
         {
+            if (contact.Count==0)                                       // this if statement shows that there is nothing in the list
+            {
+                Console.WriteLine("Currently there are no people added in your addressbook.");
+            }
+            else
+            {
+                Console.WriteLine("Here is the list and details of all the contacts in your addressbook.");
+
+                foreach (var Details in contact)                  //this foreacch loops iterates through all the contacts objects in the contacts class
+                {
+                    //this returns the variables that we have stored 
+                    Console.WriteLine("First Name -" + Details.firstName);
+                    Console.WriteLine("Last Name -" + Details.lastName);
+                    Console.WriteLine("Address -" + Details.address);
+                    Console.WriteLine("Phone Number - " + Details.phoneNumber);
+                    Console.WriteLine("Email ID -" + Details.email);
+                    Console.WriteLine("City -" + Details.city);
+                    Console.WriteLine("State -" + Details.state);
+                    Console.WriteLine("ZIP code -" + Details.zip);
+                    Console.WriteLine("-----------------------------------------------------------");
+                }
+            }
+
+        }
+
+        public void Edit()                          //this method lets the user edit the details based on their firstname
+        {
+            Console.WriteLine("Enter the first name of the contact you want to Modify.");
+            Console.WriteLine();
+            string fname = Console.ReadLine();      // taking the input of first name
             foreach (var Details in contact)
             {
-                Console.WriteLine("First Name -" + Details.firstName);
-                Console.WriteLine("Last Name -" + Details.lastName);
-                Console.WriteLine("Address -" + Details.address);
-                Console.WriteLine("Phone Number -" + Details.phoneNumber);
-                Console.WriteLine("Email ID -" + Details.email);
-                Console.WriteLine("City -" + Details.city);
-                Console.WriteLine("State -" + Details.state);
-                Console.WriteLine("ZIP code -" + Details.zip);
-                Console.WriteLine("-----------------------------------------------------------");
+                if (fname == Details.firstName)
+                {
+                    // below codes are similar to that of adding a contact.
+                    Console.Write("Enter First Name - ");
+                    Details.firstName = Console.ReadLine();
+                    Console.Write("Enter Last Name - ");
+                    Details.lastName = Console.ReadLine();
+                    Console.Write("Enter Address - ");
+                    Details.address = Console.ReadLine();
+                    Console.Write("Enter Phone number - ");
+                    Details.phoneNumber = Console.ReadLine();
+                    Console.Write("Enter Email ID - ");
+                    Details.email = Console.ReadLine();
+                    Console.Write("Enter City - ");
+                    Details.city = Console.ReadLine();
+                    Console.Write("Enter State - ");
+                    Details.state = Console.ReadLine();
+                    Console.Write("Enter ZIP code - ");
+                    Details.zip = Console.ReadLine();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("No such entry found. Please check and try again!");
+                    break;
+                }
             }
 
 
--- AddressBook01/Program.cs
+++ AddressBook01/Program.cs
@@ -10,26 +10,38 @@ namespace AddressBook01
         {
             //Class variable declaration
             int num;
+            string keyPress ="o" ;
             //below lines of code is executed at the begining to guide the user to enter their choice
             Console.WriteLine("*****Welcome to the address book program*****");
             Console.WriteLine();
             Console.WriteLine("Select the option that you would like to perform.");
-            Console.WriteLine("1- Add contact, 2- View contact");
-            num = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine();
+            //declaring address book object to be used in the below cases
+            AddressBook Person = new AddressBook();
 
-            switch(num)                                            //this switch case selects or enables the user to select multiple cases
+            while (keyPress != "n")
             {
-                case 1:
-                    AddressBook myobj1 = new AddressBook();
-                    myobj1.AddAddress();
-                    myobj1.View();
-                    break;
+                Console.WriteLine("1- Add contact, 2- View all contacts, 3- Edit contact");
+                num = Convert.ToInt32(Console.ReadLine());
+                
+                switch (num)                                            //this switch case selects or enables the user to select multiple cases
+                {
+                    case 1:
+                        Person.AddAddress();                            //method to add a new contact
+                        break;
 
-                case 2:
-                    AddressBook myobj2 = new AddressBook();
-                    myobj2.View();
-                    break;
+                    case 2:
+                        Person.View();                                  //method invoking to view all the contacts
+                        break;
+
+                    case 3:
+                        Person.Edit();                                  //method to edit the contacts
+                        break;
+
+                }
+                Console.WriteLine("Do you wish to continue?----- Press (y/n)");
+                keyPress = Console.ReadLine();
+
             }
         }
 
